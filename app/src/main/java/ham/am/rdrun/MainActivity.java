130ddb309c107a2000c69254a6baa6ed5d8f9e64
AndroidHAM <am_conan@hotmail.com>
2016-08-31 13:20:34 +0700

    load image from url

--- app/src/main/java/ham/am/rdrun/MainActivity.java
+++ app/src/main/java/ham/am/rdrun/MainActivity.java
@@ -4,13 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        //ชื่อตัวแปร = พิมพ์ finv แล้วเลื่อก findViewById
+        //จากนั้นใส่ R.id.imageView6 จากนั้น alt + Enter เลือก cast
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText =  (EditText) findViewById(R.id.editText6);
+
+        //Load Image
+        Picasso.with(this)
+                .load("http://swiftcodingthai.com/rd/Image/rd_logo.png")
+                .resize(150,150).into(imageView);
+
     }   // Main Method
 
     public void clickSignInMain(View view) {
