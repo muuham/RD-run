45eebb8c23fd8f70b4dc2717d6a935b303062209
AndroidHAM <am_conan@hotmail.com>
2016-09-01 14:11:08 +0700

    set Map finish

--- app/src/main/java/ham/am/rdrun/ServiceActivity.java
+++ app/src/main/java/ham/am/rdrun/ServiceActivity.java
@@ -20,6 +20,7 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     private ImageView imageView;
     private TextView nameTextView, surnameTextView;
     private int[] avataInts;
+    private double userLatADouble = 13.8063773, userLngADouble = 100.5754295;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,9 +72,12 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Set up Center Map
+        //กำหนด ค่า private double userLatADouble = 13.8063773, userLngADouble = 100.5754295 ไว้
+        LatLng latLng = new LatLng(userLatADouble, userLngADouble);
+        //Zoom 16 เท่า
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+
+
     }//Method onMapReady
 }//Main Class ServiceActivity
